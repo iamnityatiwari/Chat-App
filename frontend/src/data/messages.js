2e3929ea36b4a8be3ded58d8443f461da185ef3e
iamnityatiwari <149710274+iamnityatiwari@users.noreply.github.com>
2025-11-16 00:02:36 +0530

    Refactor message IDs and update content formatting

--- frontend/src/data/messages.js
+++ frontend/src/data/messages.js
@@ -1,142 +1,131 @@
 export const messages = [
   {
-    readBy: [],
-    _id: "60a3f2ed0c8dcc43bc3cfff2",
+    _id: "msg1",
     sender: {
-      pic: "https://icon-library.com/images/anonymous-avatar-icon/anonymous-avatar-icon-25.jpg",
       _id: "1",
       name: "Piyush",
+      pic: "https://icon-library.com/images/anonymous-avatar-icon/anonymous-avatar-icon-25.jpg",
     },
     content: "Hello There",
     chat: "60a3de1ff381d830b884998d",
-    createdAt: "2021-05-18T17:01:33.332Z",
-    updatedAt: "2021-05-18T17:01:33.332Z",
-    __v: 0,
-  },
-  {
     readBy: [],
-    _id: "60a3f47ece619262c884b77d",
+    createdAt: "2021-05-18T17:01:33.332Z",
+  },
+
+  {
+    _id: "msg2",
     sender: {
-      pic: "https://icon-library.com/images/anonymous-avatar-icon/anonymous-avatar-icon-25.jpg",
       _id: "2",
       name: "John Doe",
+      pic: "https://icon-library.com/images/anonymous-avatar-icon/anonymous-avatar-icon-25.jpg",
     },
     content: "Yo Wassup!",
     chat: "60a3de1ff381d830b884998d",
-    createdAt: "2021-05-18T17:08:14.447Z",
-    updatedAt: "2021-05-18T17:08:14.447Z",
-    __v: 0,
-  },
-  {
     readBy: [],
-    _id: "60a3f47ece619262c884b77d",
+    createdAt: "2021-05-18T17:08:14.447Z",
+  },
+
+  {
+    _id: "msg3",
     sender: {
-      pic: "https://icon-library.com/images/anonymous-avatar-icon/anonymous-avatar-icon-25.jpg",
       _id: "2",
       name: "John Doe",
+      pic: "https://icon-library.com/images/anonymous-avatar-icon/anonymous-avatar-icon-25.jpg",
     },
     content: "How's it going!",
     chat: "60a3de1ff381d830b884998d",
-    createdAt: "2021-05-18T17:09:14.447Z",
-    updatedAt: "2021-05-18T17:09:14.447Z",
-    __v: 0,
-  },
-  {
     readBy: [],
-    _id: "60a3f2ed0c8dcc43bc3cfff2",
+    createdAt: "2021-05-18T17:09:14.447Z",
+  },
+
+  {
+    _id: "msg4",
     sender: {
-      pic: "https://icon-library.com/images/anonymous-avatar-icon/anonymous-avatar-icon-25.jpg",
       _id: "1",
       name: "Piyush",
+      pic: "https://icon-library.com/images/anonymous-avatar-icon/anonymous-avatar-icon-25.jpg",
     },
     content: "All good!",
     chat: "60a3de1ff381d830b884998d",
-    createdAt: "2021-05-18T17:011:33.332Z",
-    updatedAt: "2021-05-18T17:011:33.332Z",
-    __v: 0,
-  },
-  {
     readBy: [],
-    _id: "60a3f2ed0c8dcc43bc3cfff2",
+    createdAt: "2021-05-18T17:11:33.332Z",
+  },
+
+  {
+    _id: "msg5",
     sender: {
-      pic: "https://icon-library.com/images/anonymous-avatar-icon/anonymous-avatar-icon-25.jpg",
       _id: "1",
       name: "Piyush",
+      pic: "https://icon-library.com/images/anonymous-avatar-icon/anonymous-avatar-icon-25.jpg",
     },
     content: "What About You?",
     chat: "60a3de1ff381d830b884998d",
-    createdAt: "2021-05-18T17:01:12.332Z",
-    updatedAt: "2021-05-18T17:01:12.332Z",
-    __v: 0,
-  },
-  {
     readBy: [],
-    _id: "60a3f47ece619262c884b77d",
+    createdAt: "2021-05-18T17:12:12.332Z",
+  },
+
+  {
+    _id: "msg6",
     sender: {
-      pic: "https://icon-library.com/images/anonymous-avatar-icon/anonymous-avatar-icon-25.jpg",
       _id: "2",
       name: "John Doe",
+      pic: "https://icon-library.com/images/anonymous-avatar-icon/anonymous-avatar-icon-25.jpg",
     },
     content: "Same, Thank You!",
     chat: "60a3de1ff381d830b884998d",
-    createdAt: "2021-05-18T17:09:14.447Z",
-    updatedAt: "2021-05-18T17:09:14.447Z",
-    __v: 0,
-  },
-  {
     readBy: [],
-    _id: "60a3f47ece619262c884b77d",
+    createdAt: "2021-05-18T17:13:14.447Z",
+  },
+
+  {
+    _id: "msg7",
     sender: {
-      pic: "https://icon-library.com/images/anonymous-avatar-icon/anonymous-avatar-icon-25.jpg",
       _id: "2",
       name: "John Doe",
-    },
-    content: "What Techonologies are you learning these days?",
-    chat: "60a3de1ff381d830b884998d",
-    createdAt: "2021-05-18T17:09:16.447Z",
-    updatedAt: "2021-05-18T17:09:16.447Z",
-    __v: 0,
-  },
-  {
-    readBy: [],
-    _id: "60a3f2ed0c8dcc43bc3cfff2",
-    sender: {
       pic: "https://icon-library.com/images/anonymous-avatar-icon/anonymous-avatar-icon-25.jpg",
+    },
+    content: "What technologies are you learning these days?",
+    chat: "60a3de1ff381d830b884998d",
+    readBy: [],
+    createdAt: "2021-05-18T17:14:16.447Z",
+  },
+
+  {
+    _id: "msg8",
+    sender: {
       _id: "1",
       name: "Piyush",
+      pic: "https://icon-library.com/images/anonymous-avatar-icon/anonymous-avatar-icon-25.jpg",
     },
     content: "Just the MERN Stack",
     chat: "60a3de1ff381d830b884998d",
-    createdAt: "2021-05-18T17:01:18.332Z",
-    updatedAt: "2021-05-18T17:01:18.332Z",
-    __v: 0,
-  },
-  {
     readBy: [],
-    _id: "60a3f47ece619262c884b77d",
+    createdAt: "2021-05-18T17:15:18.332Z",
+  },
+
+  {
+    _id: "msg9",
     sender: {
-      pic: "https://icon-library.com/images/anonymous-avatar-icon/anonymous-avatar-icon-25.jpg",
       _id: "2",
       name: "John Doe",
+      pic: "https://icon-library.com/images/anonymous-avatar-icon/anonymous-avatar-icon-25.jpg",
     },
     content: "From Roadside Coder?",
     chat: "60a3de1ff381d830b884998d",
-    createdAt: "2021-05-18T17:19:16.447Z",
-    updatedAt: "2021-05-18T17:19:16.447Z",
-    __v: 0,
-  },
-  {
     readBy: [],
-    _id: "60a3f2ed0c8dcc43bc3cfff2",
+    createdAt: "2021-05-18T17:16:16.447Z",
+  },
+
+  {
+    _id: "msg10",
     sender: {
-      pic: "https://icon-library.com/images/anonymous-avatar-icon/anonymous-avatar-icon-25.jpg",
-      _id: "60a281bc502ec30428fb6bb0",
+      _id: "1",
       name: "Piyush",
+      pic: "https://icon-library.com/images/anonymous-avatar-icon/anonymous-avatar-icon-25.jpg",
     },
-    content: "Yea boiii..",
+    content: "Yea boiii...",
     chat: "60a3de1ff381d830b884998d",
+    readBy: [],
     createdAt: "2021-05-18T17:20:18.332Z",
-    updatedAt: "2021-05-18T17:20:18.332Z",
-    __v: 0,
   },
 ];
