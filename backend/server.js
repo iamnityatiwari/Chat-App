e155cceba6eee57a5a2293126c01f630498a8efe
iamnityatiwari <149710274+iamnityatiwari@users.noreply.github.com>
2025-11-15 16:19:02 +0530

    Modify NODE_ENV check to always serve static files

--- backend/server.js
+++ backend/server.js
@@ -25,7 +25,7 @@ app.use("/api/message", messageRoutes);
 
 const __dirname1 = path.resolve();
 
-if (process.env.NODE_ENV === "production") {
+if (process.env.NODE_ENV === "production" || true) {
   app.use(express.static(path.join(__dirname1, "/frontend/build")));
 
   app.get("*", (req, res) =>
